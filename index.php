1f0319599cef0d4e4cf2990bb1d89ea0d9fda916
Jon H <jonhartm@umich.edu>
2018-01-23 12:31:51 -0500

    Instructor can add questions which are saved in the JSON field

--- index.php
+++ index.php
@@ -8,6 +8,18 @@ $LTI = LTIX::requireData();
 
 // Handle the POST Data
 $p = $CFG->dbprefix;
+$questions = $LTI->link->getJsonKey('question', '');
+$current_question = $LTI->link->getJsonKey('cur_question', '1');
+
+if ($USER->instructor){
+  if ((isset($_POST['question'])) && isset($_POST['answer'])) {
+    $new_question = array('question'=>$_POST['question'], 'answer'=>$_POST['answer']);
+    array_push($questions, $new_question);
+    $LTI->link->setJsonKey('question', $questions);
+    $_SESSION['success'] = 'Question added';
+    header( 'Location: '.addSession('index.php') ) ;
+  }
+}
 
 // Create the view
 $OUTPUT->header();
@@ -15,8 +27,30 @@ $OUTPUT->bodyStart();
 $OUTPUT->topNav();
 $OUTPUT->flashMessages();
 
-// Body Goes here
-echo "Hello";
+if ($USER->instructor){
+  // DEBUG
+  echo '<pre>';
+  print_r($questions);
+  echo '</pre>';
+
+  for ($i=0;$i<count($questions);$i++){
+    echo "Question $i: {$questions[$i]['question']} ({$questions[$i]['answer']})<br>";
+  }
+
+  echo '<form method="post">';
+    echo '<label for="question">Enter a question:&nbsp;</label>';
+    echo '<input type="text" name="question" id="question" size=80><br>';
+    echo '<label for="answer">Enter the answer:&nbsp;</label>';
+    echo '<input type="text" name="answer" id="answer" size=20><br>';
+    echo '<input type="submit">';
+  echo '</form>';
+} else {
+  echo '<form method="post">';
+    echo '<label for="question">'.$questions[$current_question]['question'].'&nbsp;</label>';
+    echo '<input type="text" name="answer" id="answer" size=10><br>';
+    echo '<input type="submit">';
+  echo '</form>';
+}
 
 $OUTPUT->footer();
 ?>
