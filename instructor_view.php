4c3640bd486a00db7e5b7251a81f65712275ea97
Jon H <jonhartm@umich.edu>
2018-01-25 13:16:15 -0500

    moved question creation form to it's own function

--- instructor_view.php
+++ instructor_view.php
@@ -8,15 +8,11 @@ function instructor_view($questions, $answers) {
   } else {
     echo 'No Questions currently posted.';
   }
-?>
-<form method="post">
-  <label for="question">Enter a question:&nbsp;</label>
-  <input type="text" name="question" id="question" size=60>
-  <label for="answer">&nbsp;Answer:&nbsp;</label>
-  <input type="text" name="answer" id="answer" size=20><br>
-  <input type="submit">
-</form>
-<?php
+
+create_question_input_form();
+
+echo '<br><br>';
+echo '<h3>Responses so far:</h3>';
   // Show the current answers
   echo '<div id="accordion">';
   foreach (sort_answers($questions, $answers) as $key => $value) {
@@ -28,6 +24,37 @@ function instructor_view($questions, $answers) {
     echo '</p>';
   }
   echo '</div>';
+}
+
+function create_question_input_form() {
+  ?>
+  <!-- Create the input form for adding a question -->
+  <br>
+  <form method="post">
+    <label for="question_type">Question Type:&nbsp;</label>
+    <select name="question_type" disabled>
+      <option value="short_answer">Short Answer</option>
+      <option value="picture">Upload Picture</option>
+    </select>
+    <br>
+    <label for="question">Enter a question:&nbsp;</label>
+    <input type="text" name="question" id="question" size=40>
+    <label for="answer">&nbsp;Answer:&nbsp;</label>
+    <input type="text" name="answer" id="answer" size=20>
+    <label for="hint">&nbsp;Provide a Hint:&nbsp;</label>
+    <input type="text" name="hint" id="hint" size=20>
+    <br>
+    <label for="enforce_case">Case Matters?</label>
+    <input type="checkbox" name="enforce_case" id="enforce_case" disabled></input>
+    <label for="answer_type">Answer Type</label>
+    <select name="answer_type" disabled>
+      <option value="number">Numerical</option>
+      <option value="text">Short Answer</option>
+    </select>
+    <br>
+    <input type="submit">
+  </form>
+<?php
 }
 
 function sort_answers($questions, $answers) {
